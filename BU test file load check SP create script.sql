7003c13213ce13cd3a12cf3f7f252a831a6e6eb3
Dan Seyfullin <dseyfullin@evolenthealth.com>
2019-08-15 10:16:25 -0400

    Create BU test file load check SP create script.sql

---
+++ BU test file load check SP create script.sql
@@ -0,0 +1,15 @@
+USE [ProviderNetworkStaging]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+create proc [dbo].[test_BU_file_load_check]
+	@ClientID int
+	,@BatchID bigint
+as
+begin
+set nocount on
+end
+
